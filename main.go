02a8c6b53835d4363a5b5fea4004bf5eec744cbf
Matheus-Armando <141080917+Matheus-Armando@users.noreply.github.com>
2025-06-08 16:32:57 -0300

    feat(main): main go file

---
+++ main.go
@@ -0,0 +1,37 @@
+package main 
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	// env variables
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080" // Default port if not specified
+	}
+
+	router := gin.Default()
+
+	router.GET("/health", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"status": "ok"})
+	})
+
+	//TODO: add other rotues here
+
+	// Start the server
+	fmt.Printf("Server is running on port %s\n", port)
+	if err := router.Run(":" + port); err != nil {
+		log.Fatalf("Failed to start server: %v", err)
+	}
+}
